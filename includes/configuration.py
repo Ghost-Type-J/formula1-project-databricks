e270ef863f0bcc502262581aed5937e15defecfd
ghost-type-j <107253394+ghost-type-j@users.noreply.github.com>
2023-10-02 14:32:23 +0100

    Filepath shorcuts

---
+++ includes/configuration.py
@@ -0,0 +1,7 @@
+# Databricks notebook source
+raw_folder_path = "/mnt/jumayelformula1dl/raw"
+processed_folder_path = "/mnt/jumayelformula1dl/processed"
+presentation_folder_path = "/mnt/jumayelformula1dl/presentation"
+
+# COMMAND ----------
+
